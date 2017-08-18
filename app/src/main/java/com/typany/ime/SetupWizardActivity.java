04f33261f255239233659ac1580cb9c112e3eb24
yangfeng <yangfeng@sogou-inc.com>
2017-08-18 19:20:35 +0800

    dump resource and activity file.

---
+++ app/src/main/java/com/typany/ime/SetupWizardActivity.java
@@ -0,0 +1,16 @@
+package com.typany.ime;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yangfeng on 2017/8/18.
+ */
+
+public class SetupWizardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setup_wizard_layout);
+    }
+}
